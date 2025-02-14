c6a3d3769d7ce42ea59df27025f89e00ce26b891
Hirotoshi Uchida <uho02741358@gmail.com>
2025-02-14 22:55:19 +0900

    Update function-list.js

--- function-list.js
+++ function-list.js
@@ -177,32 +177,140 @@ function zebra(
     func4(time * speed4)
   );
 }
-function form(n=4, k=2,n0=0,n1=1,o=60,p=1/8,q=300) {
-	if (n % 2 === 1) {
-		return osc(o,p,q)
-			.kaleid(n)
-			.rotate(Math.PI / 2)
-			.mult(shape(n,n0,n1));
-	} else if (n % Math.pow(2, k) === 0) {
-		return osc(o,p,q)
-			.kaleid(Math.pow(2, k))
-			.rotate(Math.PI / Math.pow(2, k))
-			.mult(shape(Math.pow(2, k)));
-	} else if (n % 2 === 0) {
-		return osc()
-			.kaleid(n)
-			.rotate(Math.PI)
-			.mult(shape(n,n0,n1));
-	}
+function form(n = 4, k = 2, n0 = 0, n1 = 1, o = 60, p = 1 / 8, q = 300) {
+  if (n % 2 === 1) {
+    return osc(o, p, q)
+      .kaleid(n)
+      .rotate(Math.PI / 2)
+      .mult(shape(n, n0, n1));
+  } else if (n % Math.pow(2, k) === 0) {
+    return osc(o, p, q)
+      .kaleid(Math.pow(2, k))
+      .rotate(Math.PI / Math.pow(2, k))
+      .mult(shape(Math.pow(2, k)));
+  } else if (n % 2 === 0) {
+    return osc().kaleid(n).rotate(Math.PI).mult(shape(n, n0, n1));
+  }
 }
-function rainbow(spectrum=0) {
-	red = () => solid(1, 0, 0);
-	orange = () => solid(1, 0.5, 0);
-	yellow = () => solid(1, 1, 0);
-	green = () => solid(0, 1, 0);
-	blue = () => solid(0, 0, 1);
-	indigo = () => solid(75 / 255, 0, 130 / 255);
-	violet = () => solid(238 / 255, 130 / 255, 238 / 255);
-	let filter = [red(), orange(), yellow(), green(), blue(), indigo(), violet()];
-	return filter[spectrum];
+function rainbow(spectrum = 0) {
+  red = () => solid(1, 0, 0);
+  orange = () => solid(1, 0.5, 0);
+  yellow = () => solid(1, 1, 0);
+  green = () => solid(0, 1, 0);
+  blue = () => solid(0, 0, 1);
+  indigo = () => solid(75 / 255, 0, 130 / 255);
+  violet = () => solid(238 / 255, 130 / 255, 238 / 255);
+  let filter = [red(), orange(), yellow(), green(), blue(), indigo(), violet()];
+  return filter[spectrum];
 }
+setFunction({
+	name: 'chroma',
+	type: 'color',
+	inputs: [],
+	glsl: `
+    float maxrb = max(_c0.r, _c0.b);
+    float k = smoothstep(0.0, 1.0, (_c0.g - maxrb) * 5.0);
+    _c0.g = mix(_c0.g, maxrb * 0.8, k);
+    return vec4(_c0.rgb, 1.0 - k);
+  `
+});
+
+setFunction({
+	name: 'swirl',
+	type: 'coord',
+	inputs: [{
+		name: 'strength',
+		type: 'float',
+		default: 1.0
+	}],
+	glsl: `
+    vec2 pos = _st - vec2(0.5);
+    float angle = strength * length(pos);
+    float sinA = sin(angle);
+    float cosA = cos(angle);
+    return mat2(cosA, -sinA, sinA, cosA) * pos + vec2(0.5);
+  `
+});
+
+setFunction({
+	name: 'glitch',
+	type: 'combineCoord',
+	inputs: [{
+		name: 'amount',
+		type: 'float',
+		default: 0.05
+	}],
+	glsl: `
+    vec2 offset = vec2(
+      (fract(sin(dot(_st, vec2(12.9898, 78.233))) * 43758.5453) - 0.5) * amount,
+      (fract(cos(dot(_st, vec2(93.9898, 67.233))) * 35758.5453) - 0.5) * amount
+    );
+    return _st + offset;
+  `
+});
+
+setFunction({
+	name: 'warp',
+	type: 'combineCoord',
+	inputs: [{
+		name: 'intensity',
+		type: 'float',
+		default: 0.3
+	}],
+	glsl: `
+    vec2 uv = _st - 0.5;
+    float distortion = sin(uv.x * 10.0) * sin(uv.y * 10.0) * intensity;
+    return _st + vec2(distortion, distortion * 0.5);
+  `
+});
+setFunction({
+	name: 'polar',
+	type: 'coord',
+	inputs: [],
+	glsl: `
+    vec2 uv = _st - 0.5;
+    float r = length(uv);
+    float a = atan(uv.y, uv.x);
+    return vec2(r, a / 6.2831 + 0.5);
+  `
+});
+setFunction({
+	name: 'spiral',
+	type: 'combineCoord',
+	inputs: [{
+		name: 'twist',
+		type: 'float',
+		default: 5.0
+	}],
+	glsl: `
+    vec2 uv = _st - 0.5;
+    float angle = length(uv) * twist;
+    mat2 rot = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+    return rot * uv + 0.5;
+  `
+});
+setFunction({
+	name: 'shear',
+	type: 'combineCoord',
+	inputs: [{
+		name: 'strength',
+		type: 'float',
+		default: 0.2
+	}],
+	glsl: ` 
+    return vec2(_st.x + _st.y * strength, _st.y);
+  `
+});
+setFunction({
+	name: 'randomNoise',
+	type: 'combineCoord',
+	inputs: [{
+		name: 'scale',
+		type: 'float',
+		default: 1.0
+	}],
+	glsl: `
+    float noise = fract(sin(dot(_st, vec2(12.9898, 78.233))) * 43758.5453);
+    return _st + vec2(noise * scale, noise * scale * 0.5);
+  `
+});
